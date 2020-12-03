98c60d9565dcb64d23e9ac88f0d47528cb910d93
Maria Inês Serra <maria.serra@talkdesk.com>
2020-12-03 20:06:02 +0000

    create base Translation Management System: - base server and dependencies

---
+++ tms/README.md
@@ -0,0 +1,7 @@
+wip
+
+### How to run
+
+`` ./start.sh ``
+
+^ Builds and runs the docker container
---
+++ tms/src/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+process.on("uncaughtException", e => {
+  console.log(e);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", e => {
+  console.log(e);
+  process.exit(1);
+});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//error handler middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+});
