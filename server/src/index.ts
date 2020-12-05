42de8fa23aa5aac1ea8740e2f4f6b5167e77640e
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-12-05 19:15:02 +0000

    Connects server to redis

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response, NextFunction } from 'express';
 import submitRoutes from './routes/submit';
 import notifyRoutes from './routes/notify';
+import redisClient from './configs/redis';
 
-process.on("uncaughtException", e => {
+process.on('uncaughtException', e => {
   console.log(e);
   process.exit(1);
 });
 
-process.on("unhandledRejection", e => {
+process.on('unhandledRejection', e => {
   console.log(e);
   process.exit(1);
 });
@@ -19,11 +20,19 @@ const PORT = process.env.PORT || 3000;
 app.use('/submit', submitRoutes);
 app.use('/notify', notifyRoutes);
 
-//error handler middleware
+//error handler middleware TODO: proper error handling :D
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: err.message });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
+  console.log(`Server listening on port ${PORT}`);
+
+  redisClient.on('connect', function() {
+    console.log('Connected to redis');
+  });
+
+  redisClient.on('error', function (err: Error) {
+    console.log('Something went wrong ' + err);
+    });
 });
