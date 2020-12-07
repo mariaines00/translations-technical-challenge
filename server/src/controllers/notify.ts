30c29bf3c10349a74c654c1ef0bcd87e1a040e5d
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-12-07 00:09:01 +0000

    refactor email sending

--- server/src/controllers/notify.ts
+++ server/src/controllers/notify.ts
@@ -1,10 +1,9 @@
 import { RequestHandler, Request, Response, NextFunction } from 'express';
 
-import { transporter } from './../configs/nodemailer';
-import { Email } from './../models/Email';
 import { isEmpty } from './../utils/utils';
 import { jsonRedis } from './../configs/redis';
 import { Line } from './../models/Subtitles';
+import { sendEmail } from './../services/email';
 
 export const notifyUser: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
     if(isEmpty(req.query) || req.query.email === "") {
@@ -13,20 +12,20 @@ export const notifyUser: RequestHandler = (req: Request, res: Response, next: Ne
     }
     const mail = req.query.email!.toString();
 
-    let options: Email = {
-        from: 'translations@lengoo.com',
-        subject: 'Your translations are ready',
-        to: mail,
-        text: '<translations>'
-    }
-
     jsonRedis.get(mail, ...['lines']).then((data) => {
         if (data && data.lines as Line[]) {
-            options.text = rebuild(data.lines as Line[]);
-            sendEmail(options);
-            jsonRedis.del(mail);
-            res.sendStatus(200);
+            const text = rebuild(data.lines as Line[]);
+            
+            const reply = sendEmail(mail, text);
+            if(reply) {
+                jsonRedis.del(mail);
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(500);
+            }
+            return;
         }
+        res.sendStatus(500);
     })
     .catch(err => console.log(err));
 
@@ -38,14 +37,4 @@ function rebuild(lines: Line[]): string {
         return `${line.index} ${line.start} - ${line.end} ${line.text}`; }
     ).join("\n");
     return res;
-}
-
-function sendEmail(options: any): void {
-    transporter.sendMail(options, function(error, info){
-        if(error) {
-            //res.sendStatus(500);
-            console.log(error);
-        }
-        console.log('Message sent: ' + info.response);
-    });
 }
